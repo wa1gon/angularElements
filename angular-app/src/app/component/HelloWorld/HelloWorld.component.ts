f1fd32186909172195321634f9ff271689e92881
Darryl Wagoner <darryl.wagoner@statellite.com>
2019-06-30 19:53:35 -0500

    Working completely

--- angular-app/src/app/component/HelloWorld/HelloWorld.component.ts
+++ angular-app/src/app/component/HelloWorld/HelloWorld.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   // selector: 'app-hello-world',
@@ -6,9 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./HelloWorld.component.scss']
 })
 export class HelloWorldComponent implements OnInit {
+  @Input() title;
+  @Input() rname;
+  @Input() occupation;
+  @Input() location;
+  @Input() first;
 
+  @Output() display = new EventEmitter();
   constructor() { }
-
+  showInfo() {
+    this.display.emit(`Name: ${this.rname}
+    Occupation: ${this.occupation}
+    Based In: ${this.location}
+    First Appearance: ${this.first}`);
+  }
   ngOnInit() {
   }
 
